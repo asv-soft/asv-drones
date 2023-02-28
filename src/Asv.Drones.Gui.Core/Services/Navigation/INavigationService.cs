693f91dfc85ddd8e5e765e5c420de96fabc8a4d1
Alexey <248997@gmail.com>
2023-03-01 00:58:22 +0500

    Added abstraction of open\save file\folder dialogs to INavigationService

--- src/Asv.Drones.Gui.Core/Services/Navigation/INavigationService.cs
+++ src/Asv.Drones.Gui.Core/Services/Navigation/INavigationService.cs
@@ -1,4 +1,7 @@
-﻿namespace Asv.Drones.Gui.Core
+﻿using Avalonia.Controls;
+using Avalonia.Platform.Storage;
+
+namespace Asv.Drones.Gui.Core
 {
     public class NavigationServiceHelper
     {
@@ -7,7 +10,41 @@
 
     public interface INavigationService
     {
+        void InitStorageProvider(IStorageProvider windowStorageProvider);
         void Init(IShell shell);
+        /// <summary>
+        /// Navigate to IShell page by URI
+        /// </summary>
+        /// <param name="uri"></param>    
         void GoTo(Uri uri);
+
+        /// <summary>
+        /// Shows an open file dialog
+        /// </summary>
+        /// <param name="title">The title for the dialog</param>
+        /// <param name="suggestedStartLocation">Sets the initial location where the folder open picker looks for files to present to the user.</param>
+        /// <param name="fileTypes">Sets the collection of file types that the file open picker displays</param>
+        /// <returns>The path of the file that was opened. Null if no file was opened</returns>
+        Task<string?> ShowOpenFileDialogAsync(string title, string? suggestedStartLocation, params FilePickerFileType[] fileTypes);
+
+        /// <summary>
+        /// Shows an open folder dialog
+        /// </summary>
+        /// <param name="title">The title for the dialog</param>
+        /// <param name="suggestedStartLocation">Sets the initial location where the folder open picker looks for files to present to the user.</param>
+        /// <returns>The path of the folder that was opened. Null if no folder was opened</returns>
+        Task<string?> ShowOpenFolderDialogAsync(string title, string? suggestedStartLocation);
+
+        /// <summary>
+        /// Shows a save file dialog
+        /// </summary>
+        /// <param name="title">Sets the text that appears in the title bar of a folder dialog.</param>
+        /// <param name="suggestedStartLocation">Sets the initial location where the file open picker looks for files to present to the user</param>
+        /// <param name="suggestedFileName">Sets the file name that the file save picker suggests to the user</param>
+        /// <param name="defaultExtension">Sets the default extension to be used to save the file</param>
+        /// <param name="fileTypes"></param>
+        /// <returns>The path of the file that was saved. Null if no file was saved</returns>
+        Task<string?> ShowSaveFileDialogAsync(string title, string? suggestedStartLocation, string? suggestedFileName, string? defaultExtension, params FilePickerFileType[] fileTypes);
+
     }
 }
--- src/Asv.Drones.Gui.Core/Services/Navigation/NavigationService.cs
+++ src/Asv.Drones.Gui.Core/Services/Navigation/NavigationService.cs
@@ -2,6 +2,10 @@
 using System.ComponentModel.Composition.Hosting;
 using System.Reflection;
 using Asv.Cfg;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Platform.Storage;
+using Avalonia.Platform.Storage.FileIO;
 
 namespace Asv.Drones.Gui.Core
 {
@@ -17,29 +21,38 @@ namespace Asv.Drones.Gui.Core
     public class NavigationService: ServiceWithConfigBase<NavigationServiceConfig>,INavigationService
     {
         private readonly CompositionContainer _container;
-        private IShell _shell;
-        public const string AsvUriTheme = "asv";
+        private IShell? _shell;
+        private IStorageProvider? _windowStorageProvider;
 
         [ImportingConstructor]
         public NavigationService(CompositionContainer container,IConfiguration cfgSvc):base(cfgSvc)
         {
             _container = container;
-            
+        }
+
+        public void InitStorageProvider(IStorageProvider windowStorageProvider)
+        {
+            _windowStorageProvider = windowStorageProvider ?? throw new ArgumentNullException(nameof(windowStorageProvider));
         }
 
         public void Init(IShell shellPage)
         {
-            _shell = shellPage;
+            _shell = shellPage ?? throw new ArgumentNullException(nameof(shellPage));
         }
 
         public void GoTo(Uri link)
         {
-            if (link == null) throw new ArgumentNullException(nameof(link));
-            if (link.Scheme.Equals(AsvUriTheme) == false)
+            if (_shell == null)
             {
-                throw new Exception($"Unknown uri scheme. Want {AsvUriTheme}. Got:{link.Scheme}");
+                throw new Exception(
+                    $"The order of loading services was broken. At this point the variable {nameof(_shell)} must be initialized.");
             }
-
+            if (link == null) throw new ArgumentNullException(nameof(link));
+            if (link.Scheme.Equals(WellKnownUri.UriScheme) == false)
+            {
+                throw new Exception($"Unknown uri scheme. Want {WellKnownUri.UriScheme}. Got:{link.Scheme}");
+            }
+            
             var current = _shell.CurrentPage;
             if (current?.GetType().GetCustomAttribute<PartCreationPolicyAttribute>()!.CreationPolicy == CreationPolicy.NonShared)
             {
@@ -50,6 +63,66 @@ namespace Asv.Drones.Gui.Core
             viewModel?.SetArgs(link);
             _shell.CurrentPage = viewModel;
         }
-        
+
+        public async Task<string?> ShowOpenFileDialogAsync(string title, string? suggestedStartLocation, params FilePickerFileType[] fileTypes)
+        {
+            if (_windowStorageProvider == null)
+            {
+                throw new Exception(
+                    $"The order of loading services was broken. At this point the variable {nameof(_windowStorageProvider)} must be initialized.");
+            }
+            var file = await _windowStorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+            {
+                Title = title,
+                SuggestedStartLocation = suggestedStartLocation != null ? new BclStorageFolder(suggestedStartLocation) : null,
+                AllowMultiple = false,
+                FileTypeFilter = fileTypes.Length == 0 ? null : fileTypes,
+            });
+            var selectedItem = file.FirstOrDefault();
+            if (selectedItem == null) return null;
+            return selectedItem.TryGetUri(out var uri) == false ? null : uri.AbsolutePath;
+        }
+
+
+
+        public async Task<string?> ShowOpenFolderDialogAsync(string title, string? suggestedStartLocation)
+        {
+            if (_windowStorageProvider == null)
+            {
+                throw new Exception(
+                    $"The order of loading services was broken. At this point the variable {nameof(_windowStorageProvider)} must be initialized.");
+            }
+            if (title == null) throw new ArgumentNullException(nameof(title));
+            var file = await _windowStorageProvider.OpenFolderPickerAsync(new FolderPickerOpenOptions
+            {
+                SuggestedStartLocation = suggestedStartLocation != null ? new BclStorageFolder(suggestedStartLocation):null,
+                AllowMultiple = false,
+                Title = title
+            });
+            var selectedItem = file.FirstOrDefault();
+            if (selectedItem == null) return null;
+            return selectedItem.TryGetUri(out var uri) == false ? null : uri.AbsolutePath;
+        }
+
+        public async Task<string?> ShowSaveFileDialogAsync(string title, string? suggestedStartLocation, string? suggestedFileName, string? defaultExtension, params FilePickerFileType[] fileTypes)
+        {
+            if (_windowStorageProvider == null)
+            {
+                throw new Exception(
+                    $"The order of loading services was broken. At this point the variable {nameof(_windowStorageProvider)} must be initialized.");
+            }
+            var file = await _windowStorageProvider.SaveFilePickerAsync(new FilePickerSaveOptions
+            {
+                Title = title,
+                SuggestedStartLocation = suggestedStartLocation != null ? new BclStorageFolder(suggestedStartLocation) : null,
+                SuggestedFileName = suggestedFileName,
+                DefaultExtension = defaultExtension,
+                FileTypeChoices = fileTypes.Length == 0 ? null : fileTypes,
+                ShowOverwritePrompt = true,
+            });
+            if (file == null) return null;
+            return file.TryGetUri(out var uri) == false ? null : uri.AbsolutePath;
+        }
     }
+    
 }
