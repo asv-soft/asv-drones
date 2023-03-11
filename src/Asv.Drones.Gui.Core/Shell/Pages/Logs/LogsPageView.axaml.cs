26b77b8b7016b1dda9c88500d17677e03dd36a5e
asv-soft-u02 <u02@asv.me>
2023-03-11 19:31:43 +0300

    Forgotten to add axaml.cs

---
+++ src/Asv.Drones.Gui.Core/Shell/Pages/Logs/LogsPageView.axaml.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.Composition;
+using Avalonia.ReactiveUI;
+
+namespace Asv.Drones.Gui.Core
+{
+    [ExportView(typeof(LogsPageViewModel))]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public partial class LogsPageView : ReactiveUserControl<LogsPageViewModel>
+    {
+        public LogsPageView()
+        {
+            InitializeComponent();
+        }
+    }    
+}
+
