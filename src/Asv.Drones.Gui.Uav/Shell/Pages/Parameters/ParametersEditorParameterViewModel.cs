04e1b3f418637214106651b6b62e3728531f736b
asv-soft-u02 <u02@asv.me>
2023-03-24 09:51:15 +0300

    Implemented value description logic

--- src/Asv.Drones.Gui.Uav/Shell/Pages/Parameters/ParametersEditorParameterViewModel.cs
+++ src/Asv.Drones.Gui.Uav/Shell/Pages/Parameters/ParametersEditorParameterViewModel.cs
@@ -1,5 +1,6 @@
 using System.Reactive;
 using System.Reactive.Linq;
+using System.Text;
 using System.Windows.Input;
 using Asv.Common;
 using Asv.Drones.Gui.Core;
@@ -22,16 +23,42 @@ public class ParametersEditorParameterViewModel : ViewModelBase
     public ParametersEditorParameterViewModel(ParameterItem parameterItem) : this()
     {
         Parameter = parameterItem;
-        
+
         Value = (object)(parameterItem.Parameter.IntegerValue ?? (parameterItem.Parameter.RealValue ?? 0));
+
+        StringBuilder builder = new StringBuilder();
+
+        if (Parameter.Description.Min != null & Parameter.Description.Max != null)
+        {
+            builder.Append($"{Parameter.Description.Min} - {Parameter.Description.Max}");
+        }
+        
+        if (Parameter.Description.Increment != null)
+        {
+            builder.Append($" by {Parameter.Description.Increment}");
+        }
+        
+        if (Parameter.Description.UnitsDisplayName != null)
+        {
+            builder.Append($" {Parameter.Description.UnitsDisplayName}");
+        }
+
+        ValueDescription = builder.ToString();
     }
-    
+
+    [Reactive]
     public object Value { get; set; }
-    
+
+    [Reactive]
+    public string ValueDescription { get; set; }
+
+    [Reactive]
     public ParameterItem Parameter { get; set; }
     
+    [Reactive]
     public ICommand Write { get; set; }
-
+    
+    [Reactive]
     public ICommand Update { get; set; }
 
 }
