d594d49779714f24d3df667b41709b8cdc9b0431
asv-soft-u2 <u02@asv.me>
2023-04-20 12:05:43 +0300

    Added ObserveOn

--- src/Asv.Drones.Gui.Uav/Shell/Pages/Flight/Widgets/Uav/Attitude/AttitudeViewModel.cs
+++ src/Asv.Drones.Gui.Uav/Shell/Pages/Flight/Widgets/Uav/Attitude/AttitudeViewModel.cs
@@ -74,36 +74,42 @@ namespace Asv.Drones.Gui.Uav
                 .DisposeWith(Disposable);
             
             vehicle.Connection
+                .ObserveOn(RxApp.MainThreadScheduler)
                 .Where(_=>_.SystemId == vehicle.Identity.TargetSystemId && _.MessageId == VibrationPacket.PacketMessageId)
                 .Cast<VibrationPacket>()
                 .Subscribe(_=> VibrationX = 1 - _.Payload.VibrationX)
                 .DisposeWith(Disposable);
 
             vehicle.Connection
+                .ObserveOn(RxApp.MainThreadScheduler)
                 .Where(_=>_.SystemId == vehicle.Identity.TargetSystemId && _.MessageId == VibrationPacket.PacketMessageId)
                 .Cast<VibrationPacket>()
                 .Subscribe(_=> VibrationY = 1 - _.Payload.VibrationY)
                 .DisposeWith(Disposable);
             
             vehicle.Connection
+                .ObserveOn(RxApp.MainThreadScheduler)
                 .Where(_=>_.SystemId == vehicle.Identity.TargetSystemId && _.MessageId == VibrationPacket.PacketMessageId)
                 .Cast<VibrationPacket>()
                 .Subscribe(_=> VibrationZ = 1 - _.Payload.VibrationZ)
                 .DisposeWith(Disposable);
             
             vehicle.Connection
+                .ObserveOn(RxApp.MainThreadScheduler)
                 .Where(_=>_.SystemId == vehicle.Identity.TargetSystemId && _.MessageId == VibrationPacket.PacketMessageId)
                 .Cast<VibrationPacket>()
                 .Subscribe(_=> Clipping0 = _.Payload.Clipping0)
                 .DisposeWith(Disposable);
             
             vehicle.Connection
+                .ObserveOn(RxApp.MainThreadScheduler)
                 .Where(_=>_.SystemId == vehicle.Identity.TargetSystemId && _.MessageId == VibrationPacket.PacketMessageId)
                 .Cast<VibrationPacket>()
                 .Subscribe(_=> Clipping1 = _.Payload.Clipping1)
                 .DisposeWith(Disposable);
             
             vehicle.Connection
+                .ObserveOn(RxApp.MainThreadScheduler)
                 .Where(_=>_.SystemId == vehicle.Identity.TargetSystemId && _.MessageId == VibrationPacket.PacketMessageId)
                 .Cast<VibrationPacket>()
                 .Subscribe(_=> Clipping2 = _.Payload.Clipping2)
