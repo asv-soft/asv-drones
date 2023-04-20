4a4eea53e61b4666d1b5306d5e7f31f0b66c70e2
asv-soft-u2 <u02@asv.me>
2023-04-20 18:34:29 +0300

    Added selection highlight to same type packets

--- src/Asv.Drones.Gui.Core/Shell/Pages/PacketViewer/PacketViewerViewModel.cs
+++ src/Asv.Drones.Gui.Core/Shell/Pages/PacketViewer/PacketViewerViewModel.cs
@@ -125,10 +125,27 @@ public class PacketViewerViewModel : ViewModelBase, IShellPage
             _filtersSourceType.Items.ForEach(_=>_.UpdateRateText());
             
         }).DisposeItWith(Disposable);
+
+        this.WhenValueChanged(_ => _.SelectedPacket)
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .Subscribe(_ =>
+            {
+                if (_ != null)
+                {
+                    foreach (var packet in _packets)
+                    {
+                        packet.Highlight = false;
+                    
+                        if (packet.Type == _.Type)
+                        {
+                            packet.Highlight = true;
+                        }
+                    }    
+                }
+            })
+            .DisposeItWith(Disposable);
     }
-
     
-
     private IList<PacketMessageViewModel> ConvertPacketToMessagesAndUpdateFilters(IList<IPacketV2<IPayload>> items)
     {
         var result = new List<PacketMessageViewModel>(items.Count);
@@ -169,6 +186,7 @@ public class PacketViewerViewModel : ViewModelBase, IShellPage
     
     [Reactive] public bool IsPause { get; set; }
     [Reactive] public string SearchText { get; set; }
+    [Reactive] public PacketMessageViewModel SelectedPacket { get; set; }
 
     private bool FilterBySourcePredicate(PacketMessageViewModel vm)
     {
