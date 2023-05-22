b6756bcb7652e461d505b934e1995a8afa949778
Aleksandr Burlakov <u01@asv.me>
2023-05-22 17:29:22 +0300

    Anchor Uri fixed + added anchor rotation

--- src/Asv.Drones.Gui.Uav/Shell/Pages/Flight/Layers/Anchors/AdsbAnchor/AdsbAnchor.cs
+++ src/Asv.Drones.Gui.Uav/Shell/Pages/Flight/Layers/Anchors/AdsbAnchor/AdsbAnchor.cs
@@ -2,9 +2,7 @@ using Asv.Avalonia.Map;
 using Asv.Common;
 using Asv.Drones.Gui.Core;
 using Asv.Mavlink;
-using Asv.Mavlink.Vehicle;
 using Avalonia.Media;
-using DynamicData.Binding;
 using Material.Icons;
 
 namespace Asv.Drones.Gui.Uav;
@@ -12,9 +10,10 @@ namespace Asv.Drones.Gui.Uav;
 public class AdsbAnchor : MapAnchorBase
 {
     private readonly ILocalizationService _loc;
+    public const string UriString = FlightPageViewModel.UriString + "/layer/{0}/{1}";
     
     public AdsbAnchor(IAdsbVehicle device, ILocalizationService loc, ushort deviceFullId) 
-        : base(new Uri($"adsb/{deviceFullId}/{device.IcaoAddress}"))
+        : base(new Uri(FlightPageViewModel.UriString + $"/adsb/{deviceFullId}/{device.IcaoAddress}"))
     {
         _loc = loc;
         Size = 48;
@@ -28,6 +27,7 @@ public class AdsbAnchor : MapAnchorBase
         
         // TODO: create device subscriptions
         device.CallSign.Subscribe(_ => Title = _).DisposeItWith(Disposable);
-        device.Location.Subscribe(_=>Location = _).DisposeItWith(Disposable);
+        device.Location.Subscribe(_ => Location = _).DisposeItWith(Disposable);
+        device.Heading.Subscribe(_ => RotateAngle = _ - 45).DisposeItWith(Disposable);
     }
 }
