d6be88ca8e10ef12a1b8c8120f5c2bba6f0987ff
Aleksandr Burlakov <u01@asv.me>
2023-07-03 17:37:21 +0300

    fix(core): remove file menu items
    File menu items removed because file menu does not work on mobile platforms
    
    Asana: https://app.asana.com/0/1203851531040615/1204950231412400/f

--- src/Asv.Drones.Gui.Core/Shell/Header/TopMenu/Default/DefaultHeaderFileMenuProvider.cs
+++ src/Asv.Drones.Gui.Core/Shell/Header/TopMenu/Default/DefaultHeaderFileMenuProvider.cs
@@ -3,12 +3,12 @@ using DynamicData;
 
 namespace Asv.Drones.Gui.Core
 {
-    [Export(HeaderFileMenu.UriString,typeof(IViewModelProvider<IHeaderMenuItem>))]
+    [Export(HeaderMenuItem.UriString + "/file",typeof(IViewModelProvider<IHeaderMenuItem>))]
     [PartCreationPolicy(CreationPolicy.NonShared)]
     public class DefaultHeaderFileMenuProvider : ViewModelProviderBase<IHeaderMenuItem>
     {
         [ImportingConstructor]
-        public DefaultHeaderFileMenuProvider([ImportMany(HeaderFileMenu.UriString)]IEnumerable<IHeaderMenuItem> exportedMenuItems)
+        public DefaultHeaderFileMenuProvider([ImportMany(HeaderMenuItem.UriString + "/file")]IEnumerable<IHeaderMenuItem> exportedMenuItems)
         {
             Source.AddOrUpdate(exportedMenuItems);
         }
