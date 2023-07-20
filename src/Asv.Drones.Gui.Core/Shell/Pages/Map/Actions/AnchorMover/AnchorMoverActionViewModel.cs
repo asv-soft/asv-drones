24ed35f07f5eb6c1c19bb8d60a35145b9fb09d50
Aleksandr Burlakov <u01@asv.me>
2023-07-20 14:35:58 +0300

    feat (core): add AnchorMover functionality in Map Actions
    This commit introduces functionality to move anchors in the Map view. The code changes include the addition of AnchorMoverActionViewModel, its view, and updates to the .DotSettings file to skip namespace validation for the new files added. This is to enhance the map editing experience by enabling users to easily adjust the positions of anchors.
    
    Asana: https://app.asana.com/0/1203851531040615/1204998445929976/f

---
+++ src/Asv.Drones.Gui.Core/Shell/Pages/Map/Actions/AnchorMover/AnchorMoverActionViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows.Input;
+using Asv.Common;
+using Avalonia.Controls;
+using ReactiveUI;
+
+namespace Asv.Drones.Gui.Core;
+
+[Export(FlightPageViewModel.UriString,typeof(IMapAction))]
+[Export(PlaningPageViewModel.UriString,typeof(IMapAction))]
+[PartCreationPolicy(CreationPolicy.NonShared)]
+public class AnchorMoverActionViewModel : ViewModelBase, IMapAction
+{
+    private IMap _map;
+    
+    public AnchorMoverActionViewModel() : base("asv:shell.page.map.action.move-anchors")
+    {
+    }
+    
+    public IMapAction Init(IMap context)
+    {
+        _map = context;
+        MoveAnchorsToggleCommand = ReactiveCommand.Create(() => _map.IsInAnchorEditMode = IsEnabled)
+            .DisposeItWith(Disposable);
+        return this;
+    }
+
+    public Dock Dock { get; }
+    public int Order => 0;
+    public ICommand MoveAnchorsToggleCommand { get; set; }
+    public bool IsEnabled { get; set; }
+}
