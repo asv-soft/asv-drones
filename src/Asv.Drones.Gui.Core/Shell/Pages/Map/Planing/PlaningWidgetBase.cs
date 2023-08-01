8ac7fd24e9a5a2d47850bf9d72893563f8d95e83
asv-soft-u2 <u02@asv.me>
2023-08-01 17:38:45 +0300

    feat: add AnchorsEditor in map widgets
    AnchorsEditor has been added to the map widgets. This editor allows users to edit geo-location, represented by latitude, longitude, and altitude, of selected map items. This change enhances the user experience by providing an intuitive UI to alter the geo-location of map items which is useful in various use-cases like drone navigation, geo-plotting etc. Changes were also done to update related files which invoke the AnchorsEditor.
    
    Asana: https://app.asana.com/0/1203851531040615/1205173127546645/f

--- src/Asv.Drones.Gui.Core/Shell/Pages/Map/Planing/PlaningWidgetBase.cs
+++ src/Asv.Drones.Gui.Core/Shell/Pages/Map/Planing/PlaningWidgetBase.cs
@@ -1,6 +1,6 @@
 namespace Asv.Drones.Gui.Core
 {
-    public abstract class PlaningWidgetBase:MapWidgetBase
+    public abstract class PlaningWidgetBase : MapWidgetBase
     {
         public const string UriString = PlaningPageViewModel.UriString + ".widget";
         public static readonly Uri Uri = new(UriString);
---
+++ src/Asv.Drones.Gui.Core/Shell/Pages/Map/Widgets/AnchorsEditorWidgetProvider.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.Composition;
+using DynamicData;
+
+namespace Asv.Drones.Gui.Core;
+
+[Export(FlightPageViewModel.UriString, typeof(IViewModelProvider<IMapWidget>))]
+[Export(PlaningPageViewModel.UriString, typeof(IViewModelProvider<IMapWidget>))]
+[PartCreationPolicy(CreationPolicy.NonShared)]
+public class AnchorsEditorWidgetProvider : ViewModelProviderBase<IMapWidget>
+{
+    [ImportingConstructor]
+    public AnchorsEditorWidgetProvider(ILocalizationService loc)
+    {
+        Source.AddOrUpdate(new AnchorsEditorViewModel(loc));
+    }
+}
