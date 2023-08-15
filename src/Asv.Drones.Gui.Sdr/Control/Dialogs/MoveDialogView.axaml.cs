7bda3d1bf980fd926d31b13fee1c6228357c1471
asv-soft-u2 <u02@asv.me>
2023-08-15 19:15:17 +0300

    feat(sdr): update SdrStoreBrowserView & related models for UI improvements
    Improved UI responsiveness and usability in SdrStoreBrowserView by updating Grid settings, adding GridSplitter and rearranging elements. Rows now have a minimum height. Added button for editing and deleting records. Search bar has been updated to feedback user input dynamically. Clearer visual hierarchy and user feedback implemented to provide a more intuitive experience.
    
    In the related models, delete and move functionalities were updated, new observables and properties were added for UI feedback, and validations were improved to prevent errors due to invalid inputs. Any significant changes concerning the data processing and manipulation were made for usability and functionality improvements.
    
    Asana: https://app.asana.com/0/1203851531040615/1205237267087163/f

---
+++ src/Asv.Drones.Gui.Sdr/Control/Dialogs/MoveDialogView.axaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.Composition;
+using Asv.Drones.Gui.Core;
+using Avalonia.ReactiveUI;
+
+namespace Asv.Drones.Gui.Sdr;
+
+[ExportView(typeof(MoveDialogViewModel))]
+[PartCreationPolicy(CreationPolicy.NonShared)]
+public partial class MoveDialogView : ReactiveUserControl<MoveDialogViewModel>
+{
+    public MoveDialogView()
+    {
+        InitializeComponent();
+    }
+}
