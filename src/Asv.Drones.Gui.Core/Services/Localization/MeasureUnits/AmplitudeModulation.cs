e6a8ab0e31c170eb56b5cb56e3a0ee6bd270a323
Alexey <248997@gmail.com>
2023-11-21 16:44:35 +0500

    fix(core): fix calc amptitude modulation

--- src/Asv.Drones.Gui.Core/Services/Localization/MeasureUnits/AmplitudeModulation.cs
+++ src/Asv.Drones.Gui.Core/Services/Localization/MeasureUnits/AmplitudeModulation.cs
@@ -12,8 +12,8 @@ public class AmplitudeModulation : MeasureUnitBase<double,AmplitudeModulationUni
     private const double PercentInParts = 0.01;
     
     private static readonly IMeasureUnitItem<double, AmplitudeModulationUnits>[] _units = {
-        new DoubleMeasureUnitItem<AmplitudeModulationUnits>(AmplitudeModulationUnits.Percent,RS.AmplitudeModulation_Percent_Title,"%",true,"F2",1),
-        new DoubleMeasureUnitItem<AmplitudeModulationUnits>(AmplitudeModulationUnits.InParts,RS.AmplitudeModulation_InParts_Unit,"1",true,"F4",PercentInParts),
+        new DoubleMeasureUnitItem<AmplitudeModulationUnits>(AmplitudeModulationUnits.Percent,RS.AmplitudeModulation_Percent_Title,"%",true,"F2",PercentInParts),
+        new DoubleMeasureUnitItem<AmplitudeModulationUnits>(AmplitudeModulationUnits.InParts,RS.AmplitudeModulation_InParts_Unit,"1",true,"F4",1),
     };
     
     public AmplitudeModulation(IConfiguration cfgSvc, string cfgKey) : base(cfgSvc, cfgKey, _units)
