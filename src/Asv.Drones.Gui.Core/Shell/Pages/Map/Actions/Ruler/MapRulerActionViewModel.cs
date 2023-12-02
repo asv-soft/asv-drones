bfa1238c0ce1a85e8131cc15248daf7fa3035e93
burlakov <u01@asv.me>
2023-12-02 20:28:15 +0300

    Revert "fix: second click on ruler wil off it. Next click on map will not place a ruler point"
    This reverts commit e0de781da3e059decdd0c1dd905b34ba0544306c.

--- src/Asv.Drones.Gui.Core/Shell/Pages/Map/Actions/Ruler/MapRulerActionViewModel.cs
+++ src/Asv.Drones.Gui.Core/Shell/Pages/Map/Actions/Ruler/MapRulerActionViewModel.cs
@@ -1,7 +1,5 @@
 using System.ComponentModel.Composition;
 using Asv.Common;
-using Avalonia.Controls.Shapes;
-using DocumentFormat.OpenXml.Spreadsheet;
 using DynamicData.Binding;
 using ReactiveUI.Fody.Helpers;
 
@@ -12,11 +10,10 @@ namespace Asv.Drones.Gui.Core;
 [PartCreationPolicy(CreationPolicy.NonShared)]
 public class MapRulerActionViewModel:MapActionBase
 {
-   
+  
     [ImportingConstructor]
-    public MapRulerActionViewModel( ILogService log) : base("asv:shell.page.map.action.ruler")
+    public MapRulerActionViewModel() : base("asv:shell.page.map.action.ruler")
     {
-        
         this.WhenValueChanged(_ => _.IsRulerEnabled)
             .Subscribe(SetUpRuler)
             .DisposeItWith(Disposable);
@@ -24,54 +21,30 @@ public class MapRulerActionViewModel:MapActionBase
 
     protected override void InternalWhenMapLoaded(IMap context)
     {
-       // base.InternalWhenMapLoaded(context);
-        //SetUpRuler(false);
+        base.InternalWhenMapLoaded(context);
+        SetUpRuler(false);
     }
-        
-   static CancellationTokenSource tokenSource = new CancellationTokenSource();
-    CancellationToken token = tokenSource.Token;
-   
-   
+
     private async void SetUpRuler(bool isVisible)
     {
-        
-        
-        GeoPoint start ;
-        GeoPoint stop ;
         if (Map == null) return;
         
         var polygon = Map.Markers.FirstOrDefault(x => x is RulerPolygon) as RulerPolygon;
-
         if (polygon == null) return;
-
-
-        if (isVisible == false)
-        {
-               tokenSource.Cancel();
-                polygon.Ruler.Value.Start.OnNext(null);
-                polygon.Ruler.Value.Stop.OnNext(null);
-                polygon.Ruler.Value.IsVisible.OnNext(false);
-                 tokenSource.Dispose();
-                return;
-        }
-        
+           
         if (isVisible)
         {
-            tokenSource = new CancellationTokenSource();
-            start = await Map.ShowTargetDialog(RS.MapPageViewModel_RulerStartPoint_Description, token);
-            if (tokenSource.IsCancellationRequested == true)
-            {
-                return;
-            }
-            stop = await Map.ShowTargetDialog(RS.MapPageViewModel_RulerStopPoint_Description, token);
+            var start = await Map.ShowTargetDialog(RS.MapPageViewModel_RulerStartPoint_Description, CancellationToken.None);
+            var stop = await Map.ShowTargetDialog(RS.MapPageViewModel_RulerStopPoint_Description, CancellationToken.None);
+
             polygon.Ruler.Value.Start.OnNext(start);
             polygon.Ruler.Value.Stop.OnNext(stop);
         }
+            
         polygon.Ruler.Value.IsVisible.OnNext(isVisible);
     }
     
     [Reactive]
-    
     public bool IsRulerEnabled { get; set; }
     
 }
