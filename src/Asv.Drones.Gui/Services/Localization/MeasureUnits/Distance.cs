7c6b73589e52cef538139bb4a63bab9be7b302c1
Maxim <u04@asv.me>
2024-07-26 16:52:03 +0300

    fix(MeasureUnits): add implantation in Distance
    asana: https://app.asana.com/0/1206118975886649/1207784235084607/f

--- src/Asv.Drones.Gui/Services/Localization/MeasureUnits/Distance.cs
+++ src/Asv.Drones.Gui/Services/Localization/MeasureUnits/Distance.cs
@@ -24,10 +24,54 @@ public class Distance : MeasureUnitBase<double, DistanceUnits>
     public override string Title => RS.Distance_Title;
     public override string Description => RS.Distance_Description;
 
+
+}
+
+public class DistanceMeters : IMeasureUnitItem<double, DistanceUnits>
+{
+    public DistanceUnits Id { get; }
+    public string Title { get; } = RS.Distance_Meters_Title;
+    public string Unit { get; } = RS.Distance_Meters_Unit;
+    public bool IsSiUnit { get; } = false;
+    
+    //Not usable
+    public double ConvertFromSi(double siValue)
+    {
+        return siValue;
+    }
+    //Not usable
+    public double ConvertToSi(double value)
+    {
+        return value;
+    }
+    
+    //Not usable
+    public double Parse(string? value)
+    {
+        return value != null && double.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out var result) ? result : double.NaN;
+    }
+
     public bool IsValid(string? value)
     {
         if (string.IsNullOrWhiteSpace(value)) return false;
         double.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out var v);
-        return (v > 0);
+        return v >= 0;
     }
+    
+    //Not usable
+    public string? GetErrorMessage(string? value)
+    {
+        return value;
+    }
+    //Not usable
+    public string Print(double value)
+    {
+        return $"{value}";
+    }
+    //Not usable
+    public string PrintWithUnits(double value)
+    {
+        return $"{value} {Unit}";
+    }
+
 }
