2301b2ec9514eb223185f545c71b51ae02432d27
Maxim <153065391+asv-soft-u04@users.noreply.github.com>
2024-08-06 11:34:03 +0300

    fix(MeasureUnits): add and refactor additional params in Distance (#326)
    * fix(MeasureUnits): add and refactor additional params in Distance
    
    asana: https://app.asana.com/0/1206118975886649/1207929570273909/f
    
    * fix(MeasureUnits): refactor method GetErrorMessage and add localization in Distance
    
    asana: https://app.asana.com/0/1206118975886649/1207929570273909/f

--- src/Asv.Drones.Gui/Services/Localization/MeasureUnits/Distance.cs
+++ src/Asv.Drones.Gui/Services/Localization/MeasureUnits/Distance.cs
@@ -1,21 +1,16 @@
 ﻿using System.Globalization;
 using Asv.Cfg;
+using Asv.Common;
 using Asv.Drones.Gui.Api;
-using Avalonia.Controls.Documents;
 
 namespace Asv.Drones.Gui;
 
 public class Distance : MeasureUnitBase<double, DistanceUnits>
 {
-    private const double MetersInInternationalNauticalMile = 1852;
-
     private static readonly IMeasureUnitItem<double, DistanceUnits>[] _units =
     {
-        new DistanceMeasureUnit(),
-        new DoubleMeasureUnitItem<DistanceUnits>(DistanceUnits.Meters, RS.Distance_Meters_Title,
-            RS.Distance_Meters_Unit, true, "F0", 1),
-        new DoubleMeasureUnitItem<DistanceUnits>(DistanceUnits.NauticalMiles, RS.Distance_NauticalMiles_Title,
-            RS.Distance_NauticalMiles_Unit, false, "F4", MetersInInternationalNauticalMile)
+        new DistanceMeasureUnitMeters(),
+        new DistanceMeasureUnitNauticalMiles()
     };
 
     public Distance(IConfiguration cfgSvc, string cfgKey) : base(cfgSvc, cfgKey, _units)
@@ -25,19 +20,19 @@ public class Distance : MeasureUnitBase<double, DistanceUnits>
     public override string Title => RS.Distance_Title;
     public override string Description => RS.Distance_Description;
 }
-public class DistanceMeasureUnit : IMeasureUnitItem<double, DistanceUnits>
+public class DistanceMeasureUnitMeters : IMeasureUnitItem<double, DistanceUnits>
 {
     public DistanceUnits Id => DistanceUnits.Meters;
     public string Title { get; } = RS.Distance_Meters_Title;
     public string Unit { get; } = RS.Distance_Meters_Unit;
     public bool IsSiUnit { get; } = true;
     
-    //Not usable
+    
     public double ConvertFromSi(double siValue)
     {
         return siValue;
     }
-    //Not usable
+    
     public double ConvertToSi(double value)
     {
         return value;
@@ -56,19 +51,74 @@ public class DistanceMeasureUnit : IMeasureUnitItem<double, DistanceUnits>
         return v >= 0;
     }
     
-    //Not usable
     public string? GetErrorMessage(string? value)
     {
-        return value;
+        if (value.IsNullOrWhiteSpace()) return Api.RS.DistanceMeasureUnit_ErrorMessage_NullOrWhiteSpace;
+        if (!IsValid(value)) return Api.RS.DistanceMeasureUnit_ErrorMessage_NotValid;
+        value = value.Replace(',', '.');
+        return double.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out _) == false
+            ? Api.RS.DistanceMeasureUnit_ErrorMessage_NotANumber
+            : null;
     }
-    //Not usable
+    
     public string Print(double value)
     {
-        return $"{value}";
+        return $"{value:F0}";
     }
-    //Not usable
+    
     public string PrintWithUnits(double value)
     {
-        return $"{value} {Unit}";
+        return $"{value:F0} {Unit}";
+    }
+}
+public class DistanceMeasureUnitNauticalMiles : IMeasureUnitItem<double, DistanceUnits>
+{
+    private const double MetersInInternationalNauticalMile = 1852;
+    public DistanceUnits Id => DistanceUnits.NauticalMiles;
+    public string Title { get; } = RS.Distance_NauticalMiles_Title;
+    public string Unit { get; } = RS.Distance_NauticalMiles_Unit;
+    public bool IsSiUnit { get; } = false;
+    
+    public double ConvertFromSi(double siValue)
+    {
+        return siValue / MetersInInternationalNauticalMile;
+    }
+    
+    public double ConvertToSi(double value)
+    {
+        return value * MetersInInternationalNauticalMile;
+    }
+    
+    //Not usable
+    public double Parse(string? value)
+    {
+        return value != null && double.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out var result) ? result : double.NaN;
+    }
+
+    public bool IsValid(string? value)
+    {
+        if (string.IsNullOrWhiteSpace(value)) return false;
+        double.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out var v);
+        return v >= 0;
+    }
+    
+    public string? GetErrorMessage(string? value)
+    {
+        if (value.IsNullOrWhiteSpace()) return Api.RS.DistanceMeasureUnit_ErrorMessage_NullOrWhiteSpace;
+        if (!IsValid(value)) return Api.RS.DistanceMeasureUnit_ErrorMessage_NotValid;
+        value = value.Replace(',', '.');
+        return double.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out _) == false
+            ? Api.RS.DistanceMeasureUnit_ErrorMessage_NotANumber
+            : null;
+    }
+    
+    public string Print(double value)
+    {
+        return $"{value:F4}";
+    }
+    
+    public string PrintWithUnits(double value)
+    {
+        return $"{value:F4} {Unit}";
     }
 }
