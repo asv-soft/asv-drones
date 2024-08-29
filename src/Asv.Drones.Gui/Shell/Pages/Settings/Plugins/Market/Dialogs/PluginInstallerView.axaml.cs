0ee5cca035d3f58043dd1c62d1ea272f52707e65
lev <u05asv.me>
2024-08-29 14:30:39 +0300

    feat: add manual plugin installation Asana: https://app.asana.com/0/1203851531040615/1208157082872507/f

---
+++ src/Asv.Drones.Gui/Shell/Pages/Settings/Plugins/Market/Dialogs/PluginInstallerView.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Asv.Drones.Gui.Api;
+using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+
+namespace Asv.Drones.Gui;
+
+[ExportView(typeof(PluginInstallerViewModel))]
+public partial class PluginInstallerView : ReactiveUserControl<PluginInstallerViewModel>
+{
+    public PluginInstallerView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
