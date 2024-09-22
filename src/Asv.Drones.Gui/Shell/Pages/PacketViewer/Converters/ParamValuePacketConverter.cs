9391808b1fa8b9e5272e16cfdcd203c8c43b6c67
Alexey <248997@gmail.com>
2024-09-22 20:56:44 +0500

    feat(packet-viewer): Add packet converters for ParamSet, ParamValue, and FTP packets
    Implemented new converters to handle ParamSet, ParamValue, and FTP packets in the packet viewer. These converters include functionality for interpreting and formatting packet data for display. Each converter is optimized for specific packet types using appropriate encoding methods and, where necessary, JSON serialization.

---
+++ src/Asv.Drones.Gui/Shell/Pages/PacketViewer/Converters/ParamValuePacketConverter.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Composition;
+using System.Diagnostics;
+using Asv.Drones.Gui.Api;
+using Asv.Mavlink;
+using Asv.Mavlink.V2.Common;
+
+namespace Asv.Drones.Gui;
+
+[Export(typeof(IPacketConverter))]
+public class ParamValuePacketConverter:IPacketConverter
+{
+    readonly IMavParamEncoding _cstyleEncoding = new MavParamCStyleEncoding();
+    readonly IMavParamEncoding _byteWiseEncoding = new MavParamByteWiseEncoding();
+    
+    
+    public bool CanConvert(IPacketV2<IPayload> packet)
+    {
+        return packet.MessageId == ParamValuePacket.PacketMessageId;
+    }
+
+    public string Convert(IPacketV2<IPayload> packet, PacketFormatting formatting = PacketFormatting.None)
+    {
+        var param = packet as ParamValuePacket;
+        Debug.Assert(param != null, nameof(param) + " != null");
+        
+        var name = new Span<char>(param.Payload.ParamId).TrimEnd('\0').ToString();
+        var cValue = _cstyleEncoding.ConvertFromMavlinkUnion(param.Payload.ParamValue, param.Payload.ParamType);
+        var bValue = _byteWiseEncoding.ConvertFromMavlinkUnion(param.Payload.ParamValue, param.Payload.ParamType);
+        return $"{name}[{param.Payload.ParamIndex} of {param.Payload.ParamCount}]=cstyle({cValue}) or byteWise({bValue})";
+    }
+
+    public int Order => int.MaxValue/2;
+}
