a72ecf3851c281b3252937a17f7f05119ac1c117
Ilya <shaura@asv.me>
2025-07-22 06:00:10 +0300

    feat: added facade of ftp client ex, refresh and device business logic moved into it

---
+++ src/Asv.Drones/Services/Files/IFtpClientService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Asv.Mavlink;
+using ObservableCollections;
+using R3;
+
+namespace Asv.Drones;
+
+/// <summary>
+/// Unified API for manipulating with files via FTP and
+/// for tracking remote changes.
+/// </summary>
+public interface IFtpClientService : IDisposable
+{
+    /// <summary>Gets remote side changed (added/removed/renamed).</summary>
+    Observable<Unit> RemoteChanged { get; }
+
+    /// <summary>
+    /// Gets <c>true</c> when FTP-operation executes
+    /// and <c>false</c> when client is not busy.
+    /// </summary>
+    Observable<bool> RemoteChanging { get; }
+
+    Task DownloadFileAsync(
+        string remoteFilePath,
+        string localDirectory,
+        byte partSize = MavlinkFtpHelper.MaxDataSize,
+        CancellationToken ct = default,
+        IProgress<double>? progress = null
+    );
+
+    Task BurstDownloadFileAsync(
+        string remoteFilePath,
+        string localDirectory,
+        byte partSize = MavlinkFtpHelper.MaxDataSize,
+        CancellationToken ct = default,
+        IProgress<double>? progress = null
+    );
+
+    Task UploadFileAsync(
+        string localFilePath,
+        string remoteDirectory,
+        CancellationToken ct = default,
+        IProgress<double>? progress = null
+    );
+
+    Task RemoveDirectoryAsync(string path, bool recursive = true, CancellationToken ct = default);
+
+    Task RemoveFileAsync(string path, CancellationToken ct = default);
+    Task<uint> CalculateCrc32Async(string filePath, CancellationToken ct = default);
+    Task<string> RenameAsync(string oldPath, string newName, CancellationToken ct = default);
+    Task<IReadOnlyObservableDictionary<string, IFtpEntry>> Refresh(CancellationToken ct = default);
+
+    Task CreateDirectoryAsync(string path, CancellationToken ct = default);
+}
