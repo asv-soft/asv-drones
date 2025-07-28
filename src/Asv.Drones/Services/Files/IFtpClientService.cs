4ea5cef828863376c275921603e6cb61450267ab
Ilya <shaura@asv.me>
2025-07-28 05:51:27 +0300

    feat: added methods for download/upload folders with their inner content

--- src/Asv.Drones/Services/Files/IFtpClientService.cs
+++ src/Asv.Drones/Services/Files/IFtpClientService.cs
@@ -8,17 +8,16 @@ using R3;
 namespace Asv.Drones;
 
 /// <summary>
-/// Unified API for manipulating with files via FTP and
-/// for tracking remote changes.
+/// Unified API for manipulating with files via FTP
 /// </summary>
 public interface IFtpClientService : IDisposable
 {
-    /// <summary>Gets remote side changed (added/removed/renamed).</summary>
+    /// <summary>Gets is remote side changed by this service.</summary>
     Observable<Unit> RemoteChanged { get; }
 
     /// <summary>
     /// Gets <c>true</c> when FTP-operation executes
-    /// and <c>false</c> when client is not busy.
+    /// and <c>false</c> when client is not busy anymore.
     /// </summary>
     Observable<bool> RemoteChanging { get; }
 
@@ -45,11 +44,37 @@ public interface IFtpClientService : IDisposable
         IProgress<double>? progress = null
     );
 
+    Task DownloadDirectoryAsync(
+        string remoteDirectoryPath,
+        string localDirectory,
+        byte partSize = MavlinkFtpHelper.MaxDataSize,
+        CancellationToken ct = default,
+        IProgress<double>? progress = null
+    );
+
+    Task BurstDownloadDirectoryAsync(
+        string remoteDirectoryPath,
+        string localDirectory,
+        byte partSize = MavlinkFtpHelper.MaxDataSize,
+        CancellationToken ct = default,
+        IProgress<double>? progress = null
+    );
+
+    Task UploadDirectoryAsync(
+        string localDirectoryPath,
+        string remoteDirectory,
+        CancellationToken ct = default,
+        IProgress<double>? progress = null
+    );
+
     Task RemoveDirectoryAsync(string path, bool recursive = true, CancellationToken ct = default);
 
     Task RemoveFileAsync(string path, CancellationToken ct = default);
+
     Task<uint> CalculateCrc32Async(string filePath, CancellationToken ct = default);
+
     Task<string> RenameAsync(string oldPath, string newName, CancellationToken ct = default);
+
     Task<IReadOnlyObservableDictionary<string, IFtpEntry>> Refresh(CancellationToken ct = default);
 
     Task CreateDirectoryAsync(string path, CancellationToken ct = default);
