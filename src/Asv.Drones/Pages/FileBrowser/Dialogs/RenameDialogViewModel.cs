90ea97de6d645273c177cc5f3c86a9ab3de6215d
Ilya <shaura@asv.me>
2025-08-06 08:17:22 +0300

    fix: removed unused directives

--- src/Asv.Drones/Pages/FileBrowser/Dialogs/RenameDialogViewModel.cs
+++ src/Asv.Drones/Pages/FileBrowser/Dialogs/RenameDialogViewModel.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using Asv.Avalonia;
 using Asv.Common;
 using R3;
