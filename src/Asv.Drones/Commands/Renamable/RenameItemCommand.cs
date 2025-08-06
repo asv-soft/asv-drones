5d1156ee0e5c42cc3239ef2c4d77549cd7e884e2
Ilya <shaura@asv.me>
2025-08-06 08:16:04 +0300

    feat: inline renaming feature
    still in work, function contains obvious bugs

---
+++ src/Asv.Drones/Commands/Renamable/RenameItemCommand.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Asv.Avalonia;
+using Material.Icons;
+
+namespace Asv.Drones;
+
+[ExportCommand]
+public class RenameItemCommand : ContextCommand<IRenamable, ActionArg>
+{
+    public const string Id = $"{BaseId}.rename";
+
+    private static readonly ICommandInfo StaticInfo = new CommandInfo
+    {
+        Id = Id,
+        Name = RS.RenameItemCommand_CommandInfo_Name,
+        Description = RS.RenameItemCommand_CommandInfo_Description,
+        Icon = MaterialIconKind.Pencil,
+        DefaultHotKey = HotKeyInfo.Parse("Enter"),
+        Source = SystemModule.Instance,
+    };
+
+    public override ICommandInfo Info => StaticInfo;
+
+    public override async ValueTask<ActionArg?> InternalExecute(
+        IRenamable context,
+        ActionArg arg,
+        CancellationToken cancel
+    )
+    {
+        var oldPath = arg.SubjectId;
+        var newName = arg.Value?.AsString();
+        if (string.IsNullOrWhiteSpace(oldPath) || string.IsNullOrWhiteSpace(newName))
+        {
+            return null;
+        }
+
+        var oldName = Path.GetFileName(oldPath);
+        string newPath;
+        try
+        {
+            newPath = await context.RenameItemAsync(oldPath, newName, cancel);
+        }
+        catch
+        {
+            return null;
+        }
+        return CommandArg.ChangeAction(newPath, CommandArg.CreateString(oldName));
+    }
+}
