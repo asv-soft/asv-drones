0557d288520d928f9b8bcc27d2e9b063c86f536f
Ilya <shaura@asv.me>
2025-08-08 12:50:59 +0300

    feat(ftp-browser): fixed inline-renaming bugs, improved item renaming and tree view behaviors

--- src/Asv.Drones/Pages/FileBrowser/FileBrowserView.axaml.cs
+++ src/Asv.Drones/Pages/FileBrowser/FileBrowserView.axaml.cs
@@ -2,6 +2,7 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.VisualTree;
 
 namespace Asv.Drones;
@@ -40,5 +41,42 @@ public partial class FileBrowserView : UserControl
         }
 
         return v is TreeViewItem;
+    }
+}
+
+public static class TreeViewBehaviors
+{
+    public static readonly AttachedProperty<bool> IgnoreEnterProperty =
+        AvaloniaProperty.RegisterAttached<TreeView, bool>("IgnoreEnter", typeof(TreeViewBehaviors));
+
+    static TreeViewBehaviors()
+    {
+        IgnoreEnterProperty.Changed.AddClassHandler<TreeView>(OnIgnoreEnterChanged);
+    }
+
+    public static void SetIgnoreEnter(TreeView element, bool value) =>
+        element.SetValue(IgnoreEnterProperty, value);
+
+    public static bool GetIgnoreEnter(TreeView element) => element.GetValue(IgnoreEnterProperty);
+
+    private static void OnIgnoreEnterChanged(TreeView tree, AvaloniaPropertyChangedEventArgs e)
+    {
+        var enabled = e.GetNewValue<bool>();
+        if (enabled)
+        {
+            tree.AddHandler(InputElement.KeyDownEvent, OnTreeKeyDown, RoutingStrategies.Tunnel);
+        }
+        else
+        {
+            tree.RemoveHandler(InputElement.KeyDownEvent, OnTreeKeyDown);
+        }
+    }
+
+    private static void OnTreeKeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+        {
+            e.Handled = true;
+        }
     }
 }
