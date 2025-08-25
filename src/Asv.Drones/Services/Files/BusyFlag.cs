fae8a8a276149efdb0d936c2289d5013a483adec
Ilya <shaura@cursir.com>
2025-08-25 19:56:14 +0300

    feat(ftp-browser): pr fixes, better code reusing, entries disposing and naming

---
+++ src/Asv.Drones/Services/Files/BusyFlag.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using R3;
+
+namespace Asv.Drones;
+
+public sealed class BusyFlag : IDisposable
+{
+    private readonly Subject<int> _delta = new();
+    public Observable<bool> IsBusy { get; }
+
+    public BusyFlag()
+    {
+        IsBusy = _delta
+            .Scan(0, static (acc, d) => acc + d)
+            .Select(static x => x > 0)
+            .Prepend(false)
+            .DistinctUntilChanged()
+            .Publish()
+            .RefCount();
+    }
+
+    public IDisposable Enter()
+    {
+        _delta.OnNext(+1);
+        return Disposable.Create(() => _delta.OnNext(-1));
+    }
+
+    public void Dispose() => _delta.OnCompleted();
+}
