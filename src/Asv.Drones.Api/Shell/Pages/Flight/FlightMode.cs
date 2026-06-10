ff13355e81a90b5c5776f3f4967fffbe84efa955
leomyts <leomyts@gmail.com>
2026-06-10 15:50:28 +0300

    chore: remove old flight mode artifacts from the api

--- src/Asv.Drones.Api/Shell/Pages/Flight/FlightMode.cs
+++
@@ -1,9 +0,0 @@
-using Material.Icons;
-
-namespace Asv.Drones.Api;
-
-public static class FlightMode
-{
-    public static MaterialIconKind PageIcon { get; set; } = MaterialIconKind.MapSearch;
-    public const string PageId = "flight";
-}
--- src/Asv.Drones.Api/Shell/Pages/Flight/Widgets/UavWidget/IUavFlightWidget.cs
+++
@@ -1,9 +0,0 @@
-﻿using Asv.Avalonia;
-using Asv.IO;
-
-namespace Asv.Drones.Api;
-
-public interface IUavFlightWidget : IWorkspaceWidget
-{
-    IClientDevice Device { get; }
-}
